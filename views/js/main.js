7da0685d6303e90617fce611db5ea2f6c0fc0f8f
Denyn Crawford <kiquerangelb@gmail.com>
2020-01-29 19:38:07 -0300

    Deprecating JQuery

--- views/js/main.js
+++ views/js/main.js
@@ -1,26 +1,42 @@
 
-$("#toggleMenu").on("click", () => {
-  $(".nav").toggleClass("close");
-  $("#toggleMenu i").toggleClass("hj-chevron-left");
-  $("#toggleMenu i").toggleClass("hj-chevron-right");
-  $(".section-header").toggleClass("down")
+var toggleMenu = document.querySelector("#toggleMenu");
+var nav = document.querySelector(".nav");
+var headers = document.querySelectorAll(".section-header");
+var content = document.querySelector(".content");
+var menuItem = document.querySelectorAll(".menu-item");
+var section = document.querySelectorAll(".section");
+
+toggleMenu.addEventListener("click", e => {
+  nav.classList.toggle("close");
+  toggleMenu.children[0].classList.toggle("hj-chevron-left");
+  toggleMenu.children[0].classList.toggle("hj-chevron-right");
+  for (var i = 0; i < headers.length; i++) {
+    headers[i].classList.toggle("down");
+  }
 })
 
-$(".content").click(e => {
-  $(".nav").addClass("close");
-  $("#toggleMenu i").removeClass("hj-chevron-left");
-  $("#toggleMenu i").addClass("hj-chevron-right");
-  $(".section-header").addClass("down")
-})
+content.addEventListener("click", e => {
+  nav.classList.add("close");
+  toggleMenu.children[0].classList.remove("hj-chevron-left");
+  toggleMenu.children[0].classList.add("hj-chevron-right");
+  for (var i = 0; i < headers.length; i++) {
+    headers[i].classList.add("down");
+  }
+});
 
-$(".menu-item").click(e => {
-  $(".nav").addClass("close");
-  $("#toggleMenu i").removeClass("hj-chevron-left");
-  $("#toggleMenu i").addClass("hj-chevron-right");
-  $(".section-header").addClass("down");
-  var dis = e.target;
-  var rel = dis.getAttribute('rel');
-  console.log(rel);
-  $(".section").removeClass("active");
-  $("#"+rel).addClass("active");
-})
+for (var i = 0; i < menuItem.length; i++) {
+  menuItem[i].addEventListener("click", e => {
+    nav.classList.add("close");
+    toggleMenu.children[0].classList.remove("hj-chevron-left");
+    toggleMenu.children[0].classList.add("hj-chevron-right");
+    for (var i = 0; i < headers.length; i++) {
+      headers[i].classList.add("down");
+    }
+    var dis = e.target;
+    var rel = dis.getAttribute('rel');
+    for (var i = 0; i < section.length; i++) {
+      section[i].classList.remove("active");
+    }
+    document.querySelector("#"+rel).classList.add("active");
+  });
+}
