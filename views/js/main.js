a033d74a73bd87cf53a5458f98940bd93ff44849
Denyn Crawford <kiquerangelb@gmail.com>
2020-01-28 23:12:25 -0300

    Update 28

--- views/js/main.js
+++ views/js/main.js
@@ -3,4 +3,23 @@ $("#toggleMenu").on("click", () => {
   $(".nav").toggleClass("close");
   $("#toggleMenu i").toggleClass("hj-chevron-left");
   $("#toggleMenu i").toggleClass("hj-chevron-right");
+  $(".section-header").toggleClass("up")
+})
+
+$(".content").click(e => {
+  $(".nav").addClass("close");
+  $("#toggleMenu i").removeClass("hj-chevron-left");
+  $("#toggleMenu i").addClass("hj-chevron-right");
+  $(".section-header").removeClass("up")
+})
+
+$(".menu-item").click(e => {
+  $(".nav").addClass("close");
+  $("#toggleMenu i").removeClass("hj-chevron-left");
+  $("#toggleMenu i").addClass("hj-chevron-right");
+  $(".section-header").removeClass("up");
+  var rel = $(this).attr('rel');
+  console.log(rel);
+  $(".section").removeClass("active");
+  $("#"+rel).addClass("active");
 })
