1812cc165cdf87ede4240d796e3cbb318fb66726
Denyn Crawford <kiquerangelb@gmail.com>
2020-01-29 16:51:58 -0300

    Updating section animations

--- views/js/main.js
+++ views/js/main.js
@@ -3,22 +3,23 @@ $("#toggleMenu").on("click", () => {
   $(".nav").toggleClass("close");
   $("#toggleMenu i").toggleClass("hj-chevron-left");
   $("#toggleMenu i").toggleClass("hj-chevron-right");
-  $(".section-header").toggleClass("up")
+  $(".section-header").toggleClass("down")
 })
 
 $(".content").click(e => {
   $(".nav").addClass("close");
   $("#toggleMenu i").removeClass("hj-chevron-left");
   $("#toggleMenu i").addClass("hj-chevron-right");
-  $(".section-header").removeClass("up")
+  $(".section-header").addClass("down")
 })
 
 $(".menu-item").click(e => {
   $(".nav").addClass("close");
   $("#toggleMenu i").removeClass("hj-chevron-left");
   $("#toggleMenu i").addClass("hj-chevron-right");
-  $(".section-header").removeClass("up");
-  var rel = $(this).attr('rel');
+  $(".section-header").addClass("down");
+  var dis = e.target;
+  var rel = dis.getAttribute('rel');
   console.log(rel);
   $(".section").removeClass("active");
   $("#"+rel).addClass("active");
